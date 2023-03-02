4368fdbe9a9378e398bc263c93dcb6477242a355
Rajesh Vijayan <rajmvjn@gmail.com>
2023-03-02 07:44:08 +0530

    function name types in js

--- engine/functions.js
+++ engine/functions.js
@@ -36,4 +36,70 @@ const greetA = (greet) => {
 }
 
 greetA('hi', 'raju')('raj')
+
+
+a() // this will work since the memory phase its allocated with function code;
+b() // this wont work since the memory phase will not assign function instead undefined
+
+// Function statement aka Function declaration
+
+function a() {
+  console.log('a called')     /// difference between f statement and expersion is on hoisting 
+}
+a() // this will work
+
+// Function expression
+
+var b = function() {
+  console.log('b called')
+}
+b() // this will work
+
+
+
+
+
+// Function declaration
+
+// Anonymous function - function with out name and used to assign function to vairble
+// function () {
+
+// }
+
+
+
+
+ 
+// Named function experssion
+var b = function fun() {
+  console.log('b called')
+}
+b();
+fun(); // will be errored as its not create in the global scope
+
+
+
+
+// Difference between parameters and arguments
+function fk(p1, p2) {  // p1 and p2 are parameters
+
+}
+
+fk(11, 22); // 11 and 22 are arguments..
+
+
+
+
+// First class functions - ability to pass function as arguments is called first class functions, also the ability to return a function from another function
+// aka first class citizens
+function a(p1) {
+  console.log(p1)
+}
+
+a(function(){})
+
+
+
+
+// Arrow functions
  
