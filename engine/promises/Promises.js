b5fa928fc0742bed98ce7bb5f0f0184ef0759097
Rajesh Vijayan <rajmvjn@gmail.com>
2023-03-09 07:09:41 +0530

    promise

---
+++ engine/promises/Promises.js
@@ -0,0 +1,5 @@
+// Promises are objects representing eventual completion of failure of an asychronous operation
+
+// Promise object has three states - pending, fullfilled, rejected - this is inside PromiseState
+
+// PromiseResult holds the data from the async operation once its done - serializible, which converted with JSON to readable format.
