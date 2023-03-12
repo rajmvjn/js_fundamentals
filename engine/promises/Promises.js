3c847d6c07ff0fc0bce43daac3ebb440364c037a
Rajesh Vijayan <rajmvjn@gmail.com>
2023-03-12 15:04:29 +0530

    some engine flow

--- engine/promises/Promises.js
+++ engine/promises/Promises.js
@@ -3,3 +3,7 @@
 // Promise object has three states - pending, fullfilled, rejected - this is inside PromiseState
 
 // PromiseResult holds the data from the async operation once its done - serializible, which converted with JSON to readable format.
+
+// Chaining promises also help to return a promise from a then block, which will resolved in the next then :- bcos then trigger when the promise resolved.
+
+// Chaining error scenario can be induvidually handled if we attach catch block to every then block, then bloack basically looks above to the chain
